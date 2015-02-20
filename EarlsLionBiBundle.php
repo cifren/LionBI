b4d3dc013f19395af955cbddc73f40bd6c2d70ad
cifren <cifren@msn.com>
2015-02-19 23:44:24 -0600

    Creation of the dashboard

---
+++ EarlsLionBiBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Earls\LionBiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Earls\LionBiBundle\EarlsLionBiBundle
+ */
+class EarlsLionBiBundle extends Bundle
+{
+    
+}
